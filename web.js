40436481636ed9b93c317b4512cba3d59d864095
csw026 <shaokai.wen@motorolasolutions.com>
2013-07-05 13:16:23 +0000

    update to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var fs = require("fs");
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var resultstr = fs.readFileSync("./index.html");
+  var buf = new Buffer(resultstr); 
+  response.send(buf.toString("utf8");
 });
 
 var port = process.env.PORT || 5000;
